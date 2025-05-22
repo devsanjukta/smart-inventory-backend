a042e3e4ce31f1074e474dedf5a5e16f7bbf6d33
Sanzoosunar <166828959+devsanjukta@users.noreply.github.com>
2025-05-21 23:34:17 -0500

    add changes in user registeration

---
+++ SmartInventory/SmartInventory.Core/Interfaces/IUserRepository.cs
@@ -0,0 +1,15 @@
+﻿using SmartInventory.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartInventory.Core.Interfaces
+{
+    public interface IUserRepository
+    {
+        public Task<AppUser> Register(AppUser user);
+        public Task<AppUser> GetUserByEmail(string email);
+    }
+}
---
+++ SmartInventory/SmartInventory.Services/IUserService.cs
@@ -0,0 +1,16 @@
+﻿using SmartInventory.Core.DTOs;
+using SmartInventory.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartInventory.Services
+{
+    internal interface IUserService
+    {
+        public Task<AppUserResponseDTO> Register(AppUser user);
+        public Task<AppUser> GetUserByEmail(string email);
+    }
+}
